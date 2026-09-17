25114e19c23eb9aaeba1298e6dc4794c67b6e72c
Vaishnavi Yadav <vaishnaviy3110@gmail.com>
2026-09-17 23:29:18 +0530

    Add SQL queries to sort students by various criteria

---
+++ basics/order_by.sql
@@ -0,0 +1,16 @@
+USE StudentDB;
+
+-- Sort students by marks in ascending order
+SELECT *
+FROM Students
+ORDER BY Marks ASC;
+
+-- Sort students by marks in descending order
+SELECT *
+FROM Students
+ORDER BY Marks DESC;
+
+-- Sort by name alphabetically
+SELECT *
+FROM Students
+ORDER BY Name ASC;
